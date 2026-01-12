937d3c9aa005a4690612557edab95edcc327a179
Edward <eanutt1272.v2@gmail.com>
2026-01-12 20:35:25 +0000

    fix: adjust header layout for consistent styling on medium screens

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,13 +5,13 @@ export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container">
-        <div className="flex justify-between items-center border border-white/15 p-2.5 rounded-xl max-w-2xl mx-auto">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <LogoIcon className="h-8 w-8" />
             </div>
           </div>
-          <div>
+          <div className="hidden md:block">
             <nav className="flex gap-8 text-sm">
               <a href="#" className="text-white/70 hover:text-white transition">Features</a>
               <a href="#" className="text-white/70 hover:text-white transition">Developers</a>
