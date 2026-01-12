0b17426dfab9c641d547d9de76a8237960d4b74d
Edward <eanutt1272.v2@gmail.com>
2026-01-12 20:14:13 +0000

    feat: enhance button styling with gradient borders

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -13,6 +13,10 @@ export const Header = () => {
           </div>
           <div className="flex gap-4 items-center">
             <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+              <div className="absolute inset-0 ">
+                <div className="border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)] rounded-lg"></div>
+                <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)] rounded-lg"></div>
+              </div>
               <span>Join waitlist</span>
             </button>
             <MenuIcon />
