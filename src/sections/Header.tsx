0aba089afa557d7363b8716f1840ab4da39bd682
Edward <eanutt1272.v2@gmail.com>
2026-01-12 20:07:37 +0000

    feat: add basic header styling, logo, button elements

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,24 @@
+import LogoIcon from '@/assets/logo.svg';
+import MenuIcon from '@/assets/icon-menu.svg';
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="py-4 border-b border-white/15">
+      <div className="container">
+        <div className="flex justify-between">
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="h-8 w-8" />
+            </div>
+          </div>
+          <div className="flex gap-4 items-center">
+            <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+              <span>Join waitlist</span>
+            </button>
+            <MenuIcon />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
