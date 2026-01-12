cf2330123f2e50283fbcfe682a315e057d070e93
Edward <eanutt1272.v2@gmail.com>
2026-01-12 20:31:56 +0000

    feat: enhance header layout with improved navigation and styling

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,13 +3,21 @@ import MenuIcon from '@/assets/icon-menu.svg';
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15">
+    <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container">
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center border border-white/15 p-2.5 rounded-xl max-w-2xl mx-auto">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <LogoIcon className="h-8 w-8" />
             </div>
+          </div>
+          <div>
+            <nav className="flex gap-8 text-sm">
+              <a href="#" className="text-white/70 hover:text-white transition">Features</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Developers</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Pricing</a>
+              <a href="#" className="text-white/70 hover:text-white transition">Changelog</a>
+            </nav>
           </div>
           <div className="flex gap-4 items-center">
             <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
@@ -20,7 +28,7 @@ export const Header = () => {
               </div>
               <span>Join waitlist</span>
             </button>
-            <MenuIcon />
+            <MenuIcon className="md:hidden"/>
           </div>
         </div>
       </div>
